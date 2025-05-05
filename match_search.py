8adb5f03e51beb1c52319f0ad110a5f07b87d248
devchampps <abdullah.shafiq@live.com>
2025-05-06 03:41:20 +1000

    added basic similarity search

--- match_search.py
+++ match_search.py
@@ -14,3 +14,27 @@ DB_URL = "http://localhost"
 DB_PORT = "6333"
 SERVER_URL = DB_URL + ":" + DB_PORT
 COLECTION_NAME = "Jobs"
+
+OpenAI_client = OpenAI(api_key=OPENAI_API_KEY)
+Quadrant_client = QdrantClient(url=SERVER_URL)
+
+
+searchString ="Software Developer"
+searchString_embedding = OpenAI_client.embeddings.create(
+        input=str(searchString),
+        model="text-embedding-3-small"
+        )
+
+search_result = Quadrant_client.query_points(
+    collection_name="Jobs",
+    query=searchString_embedding.data[0].embedding,
+    with_payload=True,
+    limit=3
+).points
+
+
+print("type of Search result : " + str(type(search_result)))
+for item in search_result:
+    print("Similarity Match" + str(item.score) + "\n")
+    print("JOb Title" + str(item.payload["jobTitle"]) + "\n")
+
